e4cdda305441aa536fbb27561934d80af6b70dc3
Anton Betsun <bee.tzung.jr@gmail.com>
2022-03-27 23:00:53 +0300

    Fix warnings

--- app/src/main/java/com/beetzung/quizgame/ui/MainActivity.kt
+++ app/src/main/java/com/beetzung/quizgame/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.beetzung.quizgame.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
