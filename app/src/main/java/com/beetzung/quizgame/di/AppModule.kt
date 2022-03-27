bad7e955491f263e0e04c1cbdaf552907bb96904
Anton Betsun <bee.tzung.jr@gmail.com>
2022-03-27 22:43:29 +0300

    Add game API provider

--- app/src/main/java/com/beetzung/quizgame/di/AppModule.kt
+++ app/src/main/java/com/beetzung/quizgame/di/AppModule.kt
@@ -1,9 +1,11 @@
 package com.beetzung.quizgame.di
 
 import android.content.Context
-import com.beetzung.quizgame.data.SocketAPI
-import com.beetzung.quizgame.data.socket_api_impl.SocketAPIImpl
+import com.beetzung.quizgame.data.GameAPI
+import com.beetzung.quizgame.data.GameAPIImpl
 import com.beetzung.quizgame.data.Preferences
+import com.beetzung.quizgame.data.LobbyAPI
+import com.beetzung.quizgame.data.LobbyAPIImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,9 +18,13 @@ import javax.inject.Singleton
 class AppModule {
     @Singleton
     @Provides
-    fun provideSocketAPI(): SocketAPI = SocketAPIImpl()
+    fun provideGameAPI(): GameAPI = GameAPIImpl()
 
     @Singleton
     @Provides
     fun providePrefs(@ApplicationContext context: Context) = Preferences(context)
+
+    @Singleton
+    @Provides
+    fun provideLobbyAPI(): LobbyAPI = LobbyAPIImpl()
 }
